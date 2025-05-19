3d5c014b9b8050f7f3d15bb77155b638d9e5aded
redat00 <reno@redat.me>
2025-05-19 23:48:46 +0200

    fix: remove commented code from `main.go`

--- main.go
+++ main.go
@@ -5,23 +5,5 @@ import (
 )
 
 func main() {
-	//	encryptionConfig := config.EncryptionConfiguration{
-	//		EncryptionAlgorithm: "aes",
-	//	}
-	//	encryptionEngine, err := encryption.NewEncryptionEngine(encryptionConfig)
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-	//	databaseEngine := database.NewDatabaseEngine()
-	//	app := api.NewApi(
-	//		encryptionEngine,
-	//		databaseEngine,
-	//	)
-	//	app.Listen(":3000")
-	//config, err := config.GenerateConfigFromFile("config.yml")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//fmt.Printf("%+v", config)
 	cmd.Execute()
 }
